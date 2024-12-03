92b70c2e4ad139b35ac000548307d3b225ec9420
Zein Hajj-Ali <zeinhajjali@outlook.com>
2024-12-03 11:36:37 -0500

    Using default harpoon ui instead of telescope

--- lua/custom/configs/harpoon.lua
+++ lua/custom/configs/harpoon.lua
@@ -2,31 +2,45 @@ local M = {}
 
 M.setup = function()
   local harpoon = require 'harpoon'
-  harpoon:setup()
-  local conf = require('telescope.config').values
-  local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-      table.insert(file_paths, item.value)
-    end
+  harpoon:setup {
+    settings = {
+      save_on_toggle = true,
+      -- sync_on_ui_close = true,
+    },
+  }
 
-    require('telescope.pickers')
-      .new({}, {
-        prompt_title = 'Harpoon',
-        finder = require('telescope.finders').new_table {
-          results = file_paths,
-        },
-        previewer = conf.file_previewer {},
-        sorter = conf.generic_sorter {},
-      })
-      :find()
-  end
+  -- For using telescope as the search buffer
+  -- local conf = require('telescope.config').values
+  -- local function toggle_telescope(harpoon_files)
+  --   local file_paths = {}
+  --   for _, item in ipairs(harpoon_files.items) do
+  --     table.insert(file_paths, item.value)
+  --   end
+  --
+  --   require('telescope.pickers')
+  --     .new({}, {
+  --       prompt_title = 'Harpoon',
+  --       finder = require('telescope.finders').new_table {
+  --         results = file_paths,
+  --       },
+  --       previewer = conf.file_previewer {},
+  --       sorter = conf.generic_sorter {},
+  --     })
+  --     :find()
+  -- end
+  --
+  -- vim.keymap.set('n', '<C-e>', function()
+  --   toggle_telescope(harpoon:list())
+  -- end, { desc = 'Open harpoon window' })
+
+  -- For using the default search buffer
+  vim.keymap.set('n', '<C-e>', function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+  end, { desc = 'Open harpoon window' })
+
   vim.keymap.set('n', '<leader>a', function()
     harpoon:list():add()
   end, { desc = 'Add buffer to harpoon' })
-  vim.keymap.set('n', '<C-e>', function()
-    toggle_telescope(harpoon:list())
-  end, { desc = 'Open harpoon window' })
 
   -- vim.keymap.set('n', '<C-h>', function()
   --   harpoon:list():select(1)
