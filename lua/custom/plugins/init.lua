486f6589987974cc51f87099f81cd5b9fefd9388
Zein Hajj-Ali <zeinhajjali@outlook.com>
2024-12-04 13:36:49 -0500

    Added cursor trail

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -110,4 +110,16 @@ return {
       },
     },
   },
+  {
+    'sphamba/smear-cursor.nvim',
+    opts = {
+      smear_between_buffers = false,
+      -- smear_between_neighbor_lines = true,
+      legacy_computing_symbols_support = true,
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+      hide_target_hack = false,
+    },
+  },
 }
