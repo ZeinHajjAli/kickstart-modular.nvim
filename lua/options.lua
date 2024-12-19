e8e548e5c73b9d4a99468893666816347ce3bb52
Zein Hajj-Ali <zeinhajjali@outlook.com>
2024-12-19 16:29:16 -0500

    Added filetype detection for docker compose files

--- lua/options.lua
+++ lua/options.lua
@@ -88,4 +88,13 @@ vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
 vim.opt.foldlevel = 99
 
+vim.filetype.add {
+  filename = {
+    ['docker-compose.yml'] = 'yaml.docker-compose',
+    ['docker-compose.yaml'] = 'yaml.docker-compose',
+    ['compose.yml'] = 'yaml.docker-compose',
+    ['compose.yaml'] = 'yaml.docker-compose',
+  },
+}
+
 -- vim: ts=2 sts=2 sw=2 et
