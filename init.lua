b39858d1160596893db343af77c21832099815c4
Zein Hajj-Ali <zeinhajjali@outlook.com>
2025-02-10 09:52:07 -0500

    Removed non-existent import for vscode keybinds

--- init.lua
+++ init.lua
@@ -22,8 +22,5 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
--- [[ VSCode functions and bindings]]
-require 'vscode-bindings'
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
